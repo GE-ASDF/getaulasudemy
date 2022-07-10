88a859619db64a9ae3efcb8106b6a94c97064988
Anderson Souza <83799278+GE-ASDF@users.noreply.github.com>
2022-07-09 23:15:35 -0300

    Update pegarAulasUdemy.js

--- pegarAulasUdemy.js
+++ pegarAulasUdemy.js
@@ -1,11 +1,11 @@
 let list = document.querySelectorAll(".panel--content--2q9WW .unstyled-list li");
 
+list.forEach(i=>{
+    let curriculum = i.querySelector(".truncate-with-tooltip--ellipsis--2-jEx");
+    console.log(curriculum.textContent);
+});
+
 //for(let i = 0; i < list.length;i++){
 //    let curriculum = list[i].querySelector(".truncate-with-tooltip--ellipsis--2-jEx")
 //       console.log(curriculum.textContent)
 //}
-
-list.forEach(i=>{
-    let curriculum = i.querySelector(".truncate-with-tooltip--ellipsis--2-jEx")
-    console.log(curriculum.textContent)
-})
